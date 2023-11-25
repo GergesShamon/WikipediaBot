06e1b3968ee762c1bedfca4ff26e29b71f2ce44a
GergesShamon <gerges.shamoon.dev@gmail.com>
2023-11-25 14:42:42 +0000

    Unreduce pdf dimensions

--- assets/SQL/Non-free_images.sql
+++ assets/SQL/Non-free_images.sql
@@ -10,6 +10,7 @@ JOIN categorylinks AS cl ON p.page_id = cl.cl_from
 WHERE img_width > 400
 AND img_height > 400
 AND img_name NOT LIKE "%.svg"
+AND img_name NOT LIKE "%.pdf"
 AND p.page_is_redirect = 0
 AND p.page_namespace = 6
 AND cl_to = "جميع_الملفات_غير_الحرة";
