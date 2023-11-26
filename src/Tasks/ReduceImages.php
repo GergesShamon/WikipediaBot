75ccfff55201d48a6a7401ad257e6f99473f7c22
GergesShamon <gerges.shamoon.dev@gmail.com>
2023-11-26 07:54:04 +0000

    Improve performance

--- src/Tasks/ReduceImages.php
+++ src/Tasks/ReduceImages.php
@@ -12,29 +12,50 @@ class ReduceImages extends Task
     private function getImages() : array {
         return $this->query->getArray(Util::ReadFile(FOLDER_SQL . "/Non-free_images.sql"));
     }
-
+    private function getMIMEType($content) : string {
+        if ($content !== false) {
+            $finfo = finfo_open(FILEINFO_MIME_TYPE);
+            $mime_type = finfo_buffer($finfo, $content);
+            finfo_close($finfo);
+            return $mime_type;
+        }
+        return "";
+    }
+    private function checkMIMEType($mime_type) : bool {
+        if (in_array($mime_type, array("image/jpeg", "image/png", "image/gif", "image/bmp", "image/webp"))) {
+            return true;
+        }
+        return false;
+    }
     public function ReduceImage(string $filename, int $width, int $height) : void {
         Logger::info("The bot reduces the file ${filename} size.");
         $ImageInfo = Util::getImageInfo($this->api, $filename, "url");
+
         $imageData = file_get_contents($ImageInfo["url"], false, stream_context_create([
             'http' => [
                 'header' => 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
             ],
         ]));
-        $reducer = new ReduceImage($imageData);
-        $reducer->reduce(400, $filename);
-        $fileUploader = new \Bot\Service\FileUploader($this->api);
-        if ($fileUploader->upload(
-            $filename,
-            fopen(FOLDER_TMP."/".$filename,"r"),
-            "",
-            "بوت: تصغير حجم الصور غير حرة",
-            "preferences",
-            true
-        )) {
-            Logger::info("File ${filename} uploaded successfully.");
+        $MIMEType = $this->getMIMEType($imageData);
+
+        if ($this->checkMIMEType($MIMEType)) {
+            $reducer = new ReduceImage($imageData);
+            $reducer->reduce(400, $filename);
+            $fileUploader = new \Bot\Service\FileUploader($this->api);
+            if ($fileUploader->upload(
+                $filename,
+                fopen(FOLDER_TMP."/".$filename, "r"),
+                "",
+                "بوت: تصغير حجم الصور غير حرة",
+                "preferences",
+                true
+            )) {
+                Logger::info("File ${filename} uploaded successfully.");
+            } else {
+                Logger::info("The file ${filename} was not uploaded.");
+            }
         } else {
-            Logger::info("The file ${filename} was not uploaded.");
+            Logger::warning("The file ${filename} format ${MIMEType} is not supported.");
         }
     }
     public function removeFile($filename) : void {
