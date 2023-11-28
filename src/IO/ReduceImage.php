1d5f494ae300a27b5a14756cf6ac81586689bc61
GergesShamon <gerges.shamoon.dev@gmail.com>
2023-11-28 21:26:08 +0000

    Support transparent background for images

--- src/IO/ReduceImage.php
+++ src/IO/ReduceImage.php
@@ -25,16 +25,24 @@ class ReduceImage
             $newWidth = $originalWidth * ($maxDimension / $originalHeight);
         }
 
-        // Create a new blank GD image with the calculated dimensions
+        // Create a new blank GD image with the calculated dimensions and alpha channel
         $resizedImage = imagecreatetruecolor($newWidth, $newHeight);
+
+        // Enable alpha blending
+        imagesavealpha($resizedImage, true);
+
+        // Fill the background with a transparent color
+        $transparent = imagecolorallocatealpha($resizedImage, 0, 0, 0, 127);
+        imagefill($resizedImage, 0, 0, $transparent);
 
         // Resize the original image to the new dimensions
         imagecopyresampled($resizedImage, $originalImage, 0, 0, 0, 0, $newWidth, $newHeight, $originalWidth, $originalHeight);
 
+
         // Output the resized image directly to the browser
         $originalImageInfo = getimagesizefromstring($this->imageData);
         $mime = $originalImageInfo["mime"];
-        
+
         switch ($mime) {
             case "image/jpeg":
                 imagejpeg($resizedImage, FOLDER_TMP."/${filename}");
