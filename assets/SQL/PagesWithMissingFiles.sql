958d80b8918e2b80dcaa432628e3bc424c28dda8
GergesShamon <gerges.shamoon.dev@gmail.com>
2023-12-18 16:00:37 +0000

    Update PagesWithMissingFiles.sql

--- assets/SQL/PagesWithMissingFiles.sql
+++ assets/SQL/PagesWithMissingFiles.sql
@@ -5,6 +5,7 @@ FROM
 INNER JOIN categorylinks AS category ON p.page_id = category.cl_from
 WHERE
     p.page_is_redirect = 0
+    AND p.page_id in (SELECT fr_page_id FROM flaggedrevs WHERE fr_rev_id = page_latest)
     AND p.page_namespace = 0
     AND category.cl_to = "صفحات_تحوي_وصلات_ملفات_معطوبة"
 GROUP BY p.page_title
