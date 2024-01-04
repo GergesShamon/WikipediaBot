535afec981eee457c8812816027ac02963e4f96e
GergesShamon <gerges.shamoon.dev@gmail.com>
2024-01-04 23:14:26 +0000

    Update task ReduceImages

--- assets/SQL/Non-free_images.sql
+++ assets/SQL/Non-free_images.sql
@@ -14,4 +14,11 @@ AND img_name NOT LIKE "%.pdf"
 AND img_name NOT LIKE "%.tif"
 AND p.page_is_redirect = 0
 AND p.page_namespace = 6
-AND cl_to = "جميع_الملفات_غير_الحرة";
+AND cl_to = "جميع_الملفات_غير_الحرة"
+AND img_name NOT IN (
+    SELECT im.img_name
+    FROM image AS im
+    JOIN page ON cl.cl_from = page.page_id
+    JOIN categorylinks ON page.page_id = categorylinks.cl_from
+    WHERE cl_to = "ملفات_غير_حرة_موسومة_لعدم_تقليل_الدقة"
+);
